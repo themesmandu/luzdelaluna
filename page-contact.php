c9686dbc684d505ef2035f402d83e513923c142b
Saurav Shrestha <sauravxtha@gmail.com>
2019-10-02 12:39:10 +0545

    added page templates

---
+++ page-contact.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Contact custom template
+ *
+ * This is the template that displays custom contact page.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Luzdelaluna
+ */
+
+get_header();
+?>
+
+
+
+<?php
+get_footer();
---
+++ page-portfolio.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Portfolio custom template
+ *
+ * This is the template that displays custom portfolio page.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Luzdelaluna
+ */
+
+get_header();
+?>
+
+
+
+<?php
+get_footer();
