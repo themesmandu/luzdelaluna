f59f136e3c07ef7cbc9d0856761ef51d1b4d2603
Saurav Shrestha <sauravxtha@gmail.com>
2019-10-03 16:06:02 +0545

    updated shop page

--- page-shop.php
+++ page-shop.php
@@ -24,89 +24,90 @@ $downloads = new WP_Query( $args );
 
 ?>
 <div class="container">
-    <ul class="row">
+	<ul class="row">
 
-        <?php
-if ( $downloads->have_posts() ) :
-	while ( $downloads->have_posts() ) :
-		$downloads->the_post();
-		$download_id = get_the_ID();
+		<?php
+		if ( $downloads->have_posts() ) :
+			while ( $downloads->have_posts() ) :
+				$downloads->the_post();
+				$download_id = get_the_ID();
+				$download    = new EDD_Download( $download_id );
 
-		?>
-        <li class="col-md-3 column">
-            <div class="product-wrap align-center">
-                <figure>
-                    <?php the_post_thumbnail(); ?>
-                    <div class="add_cart">
-                        <?php
-		echo edd_get_purchase_link(
-			array(
-				'download_id' => $download_id,
-				'price'       => false,
-				'text'        => 'Add to cart',
-				'style'       => 'plain',
-			)
-		);
-		?>
-                    </div>
+				?>
+		<li class="col-md-3 column">
+			<div class="product-wrap align-center">
+				<figure>
+					<?php the_post_thumbnail(); ?>
+					<div class="add_cart">
+						<?php
+						echo edd_get_purchase_link(
+							array(
+								'download_id' => $download_id,
+								'price'       => false,
+								'text'        => 'Add to cart',
+								'style'       => 'plain',
+							)
+						);
+						?>
+					</div>
 
-                </figure>
+				</figure>
 
-                <h2 class="product-heading bold-5 uppercase">
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </h2>
+				<h2 class="product-heading bold-5 uppercase">
+					<a href="<?php the_permalink(); ?>"><?php echo esc_html( $download->post_title ); ?></a>
+				</h2>
 
-                <span class="price">
-                    <?php edd_price( $download_id ); ?>
-                </span>
-            </div>
-        </li>
-        <?php
-	endwhile;
-	?>
-    </ul> <!-- end of ul .row -->
+				<span class="price">
+					<?php edd_price( $download_id ); ?>
+				</span>
+			</div>
+		</li>
+				<?php
+			endwhile;
+			?>
+	</ul> <!-- end of ul .row -->
 
-    <?php
-	/**
-			 * Pagination parameters of the_posts_pagination() since: 4.1.0
-			 *
-			 * @see the_posts_pagination
-			 * https://codex.wordpress.org/Function_Reference/the_posts_pagination
-			 *
-			 */
-		$pagination_args = array(
-			'mid_size'  => 4,
-			'prev_text' => __( 'Prev', 'luzdelaluna' ),
-			'next_text' => __( 'Next', 'luzdelaluna' ),
-		);
+			<?php
+			/**
+					 * Pagination parameters of the_posts_pagination() since: 4.1.0
+					 *
+					 * @see the_posts_pagination
+					 * https://codex.wordpress.org/Function_Reference/the_posts_pagination
+					 *
+					 */
+				$pagination_args = array(
+					'mid_size'  => 4,
+					'prev_text' => __( 'Prev', 'luzdelaluna' ),
+					'next_text' => __( 'Next', 'luzdelaluna' ),
+				);
 
-		/**
-		 * Fix pagination link base
-		 *
-		 * If in paginated posts w/o multiple loop
-		 *
-		 */
+				/**
+				 * Fix pagination link base
+				 *
+				 * If in paginated posts w/o multiple loop
+				 *
+				 */
 
-		if ( ! is_front_page() && 0 < intval( $page ) ) {
-			$pagination_args['base'] = user_trailingslashit(
-				untrailingslashit( get_page_link( $page_ID ) ) . '/page/%#%'
-			);
-		}
-		/**
-		 * Fix Pagination with $GLOBALS['wp_query'] = {custom_query}
-		 *
-		 * @see get_the_posts_pagination use $GLOBALS['wp_query']
-		 * https://developer.wordpress.org/reference/functions/get_the_posts_pagination/#source-code
-		 *
-		 */
-		$GLOBALS['wp_query'] = $downloads;
-		the_posts_pagination( $pagination_args );
-		else :
-			get_template_part( 'template-parts/content', 'none' );
+				if ( ! is_front_page() && 0 < intval( $page ) ) {
+					$pagination_args['base'] = user_trailingslashit(
+						untrailingslashit( get_page_link( $page_ID ) ) . '/page/%#%'
+					);
+				}
+				/**
+				 * Fix Pagination with $GLOBALS['wp_query'] = {custom_query}
+				 *
+				 * @see get_the_posts_pagination use $GLOBALS['wp_query']
+				 * https://developer.wordpress.org/reference/functions/get_the_posts_pagination/#source-code
+				 *
+				 */
+				$GLOBALS['wp_query'] = $downloads;
+				the_posts_pagination( $pagination_args );
+				else :
+					get_template_part( 'template-parts/content', 'none' );
 endif;
-		wp_reset_postdata();
+				wp_reset_postdata();
 
-?>
+				?>
 </div> <!-- end of Container -->
 <?php
 		get_footer();
