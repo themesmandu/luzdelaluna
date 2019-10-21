4cc2d7a3aaa5e1fc35cbb97cb898181db10c033f
Rupesh Rai <51738506+rupeshchamling@users.noreply.github.com>
2019-10-21 17:18:18 +0545

    ltest changes

--- header.php
+++ header.php
@@ -13,28 +13,28 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<div id="page" class="site">
-		<header id="top-header" class="site-header" role="banner">
-			<div class="header-wrap">
-				<?php
+    <div id="page" class="site">
+        <header id="top-header" class="site-header" role="banner">
+            <div class="header-wrap">
+                <?php
 
 				get_template_part( 'template-parts/navigation/main-navbar', 'alt' );
 
 				//header page title.
 				luzdelaluna_header_page_title();
 				?>
-			</div>
+            </div>
 
-			<?php if ( is_front_page() && ! is_home() && get_theme_mod( 'slider_toggle' ) ) : ?>
-				<?php
+            <?php if ( is_front_page() && ! is_home() && get_theme_mod( 'slider_toggle' ) ) : ?>
+            <?php
 				$active_sliders = array();
 				for ( $i = 1; $i <= 6; $i++ ) {
 					if ( get_theme_mod( 'slider_image_' . $i ) ) {
@@ -43,35 +43,36 @@
 				}
 				if ( 0 !== count( $active_sliders ) ) :
 					?>
-			<div id="image_slider">
-				<div class="slider_images">
-					<ul>
-					<?php foreach ( $active_sliders as $key => $slider_id ) : ?>
-						<li>
-							<img src="<?php echo esc_url( wp_get_attachment_url( get_theme_mod( 'slider_image_' . $slider_id ) ) ); ?>"
-								alt="<?php echo esc_url( wp_get_attachment_url( get_theme_mod( 'slider_image_' . $slider_id ) ) ); ?>"/>
-								<?php if ( get_theme_mod( 'slider_title' ) || get_theme_mod( 'slider_button_label' ) || get_theme_mod( 'slider_button_link' ) ) : ?>
-							<div class="slide_caption">
-								<div class="slide_content">
-									<?php if ( get_theme_mod( 'slider_title' ) ) : ?>
-								<h1 class="bold-8 slider-heading"><?php echo wp_kses_post( get_theme_mod( 'slider_title' ) ); ?></h1>
-									<?php endif; ?>		
-							</div>
-							</div>
-							<?php endif; ?>	
-						</li>
-					<?php endforeach; ?>
-					</ul>
-				</div>
-				<div class="slider_indicator">
-					<?php foreach ( $active_sliders as $slider_id ) : ?>
-					<a href="#"></a>
-					<?php endforeach; ?>
-				</div>
-			</div>
-				<?php endif; ?>
-			<?php endif; ?>
+            <div id="image_slider">
+                <div class="slider_images">
+                    <ul>
+                        <?php foreach ( $active_sliders as $key => $slider_id ) : ?>
+                        <li>
+                            <img src="<?php echo esc_url( wp_get_attachment_url( get_theme_mod( 'slider_image_' . $slider_id ) ) ); ?>"
+                                alt="<?php echo esc_url( wp_get_attachment_url( get_theme_mod( 'slider_image_' . $slider_id ) ) ); ?>" />
+                            <?php if ( get_theme_mod( 'slider_title' ) || get_theme_mod( 'slider_button_label' ) || get_theme_mod( 'slider_button_link' ) ) : ?>
+                            <div class="slide_caption">
+                                <div class="slide_content">
+                                    <?php if ( get_theme_mod( 'slider_title' ) ) : ?>
+                                    <h1 class="bold-8 slider-heading">
+                                        <?php echo wp_kses_post( get_theme_mod( 'slider_title' ) ); ?></h1>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                            <?php endif; ?>
+                        </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+                <div class="slider_indicator">
+                    <?php foreach ( $active_sliders as $slider_id ) : ?>
+                    <a href="#"></a>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+            <?php endif; ?>
+            <?php endif; ?>
 
-		</header><!-- #masthead -->
+        </header><!-- #masthead -->
 
-		<div id="content" class="content-wrap">
+        <div id="content" class="content-wrap">
